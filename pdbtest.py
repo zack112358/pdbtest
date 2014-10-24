a5ed3c59e2486ad83b989529da154ee55a5fbc39
Zachary McCord <zjmccord@gmail.com>
2014-10-24 18:00:06 -0400

    initial commit

---
+++ pdbtest.py
@@ -0,0 +1,101 @@
+# Copyright 2014 Zachary J. McCord
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# and a copy of the license is also included in this software package.
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+from __future__ import print_function
+import unittest
+import functools
+import pdb
+import types
+import sys
+
+"""
+A simple library to be used after the manner of unittest that tries to hook
+test suites and make them invoke PDB on exceptions. For the situation where you
+don't really have control over the test code, or don't want to modifiy the test
+code, but still want to get an interactive debugger instead of a summary.
+"""
+
+
+def pdb_wrap(fun, message=None):
+    def hooked_fun(*args, **kwargs):
+        try:
+            fun(*args, **kwargs)
+        except:
+            exc_info = sys.exc_info()
+            if message is not None:
+                print(message, file=sys.stderr)
+            print(exc_info[1], file=sys.stderr)
+            pdb.post_mortem(exc_info[2])
+    return hooked_fun
+
+
+def pdb_wrap_cases(test):
+    if hasattr(test, '_tests'):
+        # print("Wrapping suite", test, file=sys.stderr)
+        for i in xrange(len(test._tests)):
+            test._tests[i] = pdb_wrap_cases(test._tests[i])
+    else:
+        # print("Wrapping test", test, file=sys.stderr)
+        testMethod = getattr(test, test._testMethodName)
+        def doc_line(docstring):
+            return "\n'" + docstring.strip() + "'" if docstring else ''
+        message = "Debugging exception in test %r%s\nmethod %r%s" % (
+            type(test), doc_line(test.__doc__),
+            test._testMethodName, doc_line(testMethod.__doc__))
+        setattr(test, test._testMethodName, pdb_wrap(testMethod, message))
+
+    return test
+
+
+class PDBRunnerMixin(object):
+    """
+    Mixin class that makes a unittest runner open PDB postmortem on exceptions
+    in tests
+    """
+    def run(self, test):
+        hooked_test = pdb_wrap_cases(test)
+        return super(PDBRunnerMixin, self).run(hooked_test)
+
+
+class TestProgram(unittest.TestProgram):
+    """
+    Subclass of unittest main that uses PDBRunnerMixin
+    """
+
+    def __init__(self,
+                 module=None,
+                 testRunner=unittest.TextTestRunner,
+                 *args, **kwargs):
+        """
+        Take the specified testRunner, mix in the PDB behavior, and continue
+        like normal
+        """
+        assert isinstance(testRunner, types.TypeType)
+        testRunner = type("PDB" + testRunner.__name__,
+                          (PDBRunnerMixin, testRunner),
+                          dict())
+        # pdb.set_trace()
+        super(TestProgram, self).__init__(*args,
+                                          testRunner=testRunner,
+                                          module=module,
+                                          **kwargs)
+
+
+main = TestProgram
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='pdbtest',
+    packages=['pdbtest'],
+    version='1.0',
+    description='A unittest shim to make PDB debugging of test cases easy.',
+    author='Zachary J. McCord',
+    author_email='zjmccord@gmail.com',
+    url='',
+    download_url='',
+    keywords=['testing', 'pdb', 'unittest'],
+    classifiers=[],
+)
