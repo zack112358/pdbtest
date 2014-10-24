b3f33489a9381f36a7eb94df92456075b35a31ae
Zachary McCord <zjmccord@gmail.com>
2014-10-24 18:27:11 -0400

    bump rev

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pdbtest',
     packages=['pdbtest'],
-    version='1.0',
+    version='1.1',
     description='A unittest shim to make PDB debugging of test cases easy.',
     author='Zachary J. McCord',
     author_email='zjmccord@gmail.com',
     url='https://github.com/zack112358/pdbtest/',
-    download_url='https://github.com/zack112358/pdbtest/tarball/1.0',
+    download_url='https://github.com/zack112358/pdbtest/tarball/1.1',
     keywords=['testing', 'pdb', 'unittest'],
     classifiers=[],
 )
