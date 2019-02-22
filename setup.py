845d7749ce93f1f1a6d8c55e27606ca918657250
Zachary McCord <zjmccord@gmail.com>
2019-02-21 22:23:48 -0400

    update pypi version

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 setup(
     name='pdbtest',
     packages=['pdbtest'],
-    version='1.1',
+    version='1.2',
     description='A unittest shim to make PDB debugging of test cases easy.',
     author='Zachary J. McCord',
     author_email='zjmccord@gmail.com',
     url='https://github.com/zack112358/pdbtest/',
-    download_url='https://github.com/zack112358/pdbtest/tarball/1.1',
     keywords=['testing', 'pdb', 'unittest'],
     classifiers=[],
 )
