e3e2c0ca68a00ae692b86eba652d1058b75fa884
Zachary McCord <zjmccord@gmail.com>
2019-03-02 22:41:44 -0600

    add pypi classifiers (bump to 1.3)

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from distutils.core import setup
 setup(
     name='pdbtest',
     packages=['pdbtest'],
-    version='1.2',
+    version='1.3',
     description='A unittest shim to make PDB debugging of test cases easy.',
     author='Zachary J. McCord',
     author_email='zjmccord@gmail.com',
     url='https://github.com/zack112358/pdbtest/',
     keywords=['testing', 'pdb', 'unittest'],
-    classifiers=[],
+    classifiers=["Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 2",
+                 "Topic :: Software Development :: Debuggers",
+                 "Topic :: Software Development :: Testing",
+                 "License :: OSI Approved :: Apache Software License"],
 )
